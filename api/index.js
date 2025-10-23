d53ce215e715921f489f2f46b062bb51c466898d
cepiseptiyana <cepifams3@gmail.com>
2025-10-23 14:14:00 +0700

    first commit

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 3001;
+
+const userRoutes = require("./routes/userRoutes");
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/users", userRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+module.exports = app;
